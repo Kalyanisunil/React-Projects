147e6ba4c4dbc6546864f66c47fb33af8e99201f
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-10-15 10:29:59 +0530

    feat:A page to display the favorite food list using variables in react

---
+++ favfoodlist/src/App.js
@@ -0,0 +1,26 @@
+
+let foodname1 = "pizza";
+let foodname2 = "icecream";
+let foodname3 = "cake";
+function App() {
+  return (
+    <div className="mx-auto" style={{width:' 200px'
+}
+}>
+      <ul className="text-decoration-none ">
+        <li>
+        {foodname1}
+        </li>
+        <li>
+        {foodname1}
+        </li>
+        <li>
+        {foodname1}
+        </li>
+      
+      </ul>
+   </div>
+  );
+}
+
+export default App;
