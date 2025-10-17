c8698821cf98743644d639c6a9eb3e7cea17d68c
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-10-17 13:04:05 +0530

    webpage to understand routing

---
+++ studentlist/src/App.js
@@ -0,0 +1,15 @@
+import Student from "./Student";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+           <h1>Student List</h1>
+           <Student/>
+      </div>
+   
+    </>
+  );
+}
+
+export default App;
---
+++ studentlist/src/Student.js
@@ -0,0 +1,12 @@
+// import { Link, NavLink } from "react-router-dom";
+function Student() {
+  return (
+    <>
+      <ul className="nav">
+        <li className="nav-item">Keerthi</li>
+      </ul>
+    </>
+  );
+}
+
+export default Student;
