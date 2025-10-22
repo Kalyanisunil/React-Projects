9b9d6d556e5d4af56ef84d415b367403879fbb41
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-10-22 11:10:56 +0530

    chefclaude:recipe maker

---
+++ chefclaude/src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header()
+{
+ return (
+     <>
+         <nav>
+             <img src="" alt="here is an image"></img>
+             <h2>Chef Claude</h2>
+         </nav>
+        </>
+    )
+}
+   
