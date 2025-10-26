b01850697b0cf483e08a5c565078516ee4e89d1c
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-10-26 20:26:37 +0530

    api crud

---
+++ catalog/src/App.js
@@ -0,0 +1,24 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Create from "./Create";
+import Update from "./Update";
+import Delete from "./Delete";
+import Read from "./Read";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/create" element={<Create />}></Route>
+        <Route path="/update/:id" element={<Update />}></Route>
+        <Route path="/delete" element={<Delete />}></Route>
+         <Route path="/read/:id" element={<Read/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
